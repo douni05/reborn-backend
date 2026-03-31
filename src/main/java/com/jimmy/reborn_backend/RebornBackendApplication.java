bdb2f7351983b368753b1089696a053fdb6a57f6
douni05 <douni05@gmail.com>
2026-03-31 15:30:48 +0900

    init:프로젝트 초기설정

---
+++ src/main/java/com/jimmy/reborn_backend/RebornBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jimmy.reborn_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebornBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebornBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimmy/reborn_backend/RebornBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmy.reborn_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebornBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
