94ebc8d53d60bbad44b1bad61511279fcdf189fa
douni05 <douni05@gmail.com>
2026-05-26 12:08:02 +0900

    fix: 불필요한 파일 삭제

--- src/main/java/com/jimmy/reborn_backend/domain/repository/DisposalHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jimmy.reborn_backend.domain.repository;
-
-import com.jimmy.reborn_backend.domain.entity.DisposalHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisposalHistoryRepository extends JpaRepository<DisposalHistory, Long> {
-}
